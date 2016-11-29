390ef9bfff81245db688576ba73c899ea8605980
chloelemon <chloe.mengli@gmail.com>
2016-11-29 18:59:18 -0500

    Edited

--- MeshDict_Fun.R
+++ MeshDict_Fun.R
@@ -9,24 +9,26 @@
 
 MeshDict <- function(dataframe){
   
+  #then do the same to meshHeadings
+  newdt <- data.table(dataframe, key="SemanticTypeName")
+  newdt <- newdt[, c(list(meshHeadings = unlist(strsplit(meshHeadings, "[|]")))), by=SemanticTypeName]
+  
+  newdt <- as.data.frame(newdt)
+  #remove duplicate
+  newdt <- unique(newdt)
+  
+  
   #set the key (unique identifier)
-  d.dt <- data.table(dataframe, key="meshHeadings") #set key to meshHeadings first
+  newdt <- data.table(newdt, key="meshHeadings") #set key to meshHeadings first
   
   #repeat the meshHeadings for each element of each SemanticType
-  newdt <- d.dt[, c(list(SemanticTypeName = unlist(strsplit(SemanticTypeName, "[|]")))), by=meshHeadings]
+  newdt <- newdt[, c(list(SemanticTypeName = unlist(strsplit(SemanticTypeName, "[|]")))), by=meshHeadings]
   #split by "|" but has to add [|] because it is a regex
   
   newdt <- as.data.frame(newdt)
   #remove duplicate so that there are no duplicate in the semantictypename cell
   newdt <- unique(newdt)
   
-  #then do the same to meshHeadings
-  newdt <- data.table(newdt, key="SemanticTypeName")
-  newdt <- newdt[, c(list(meshHeadings = unlist(strsplit(meshHeadings, "[|]")))), by=SemanticTypeName]
-  
-  newdt <- as.data.frame(newdt)
-  #remove duplicate
-  newdt <- unique(newdt)
   
   return(newdt)
   
